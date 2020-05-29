904f58b05f2583ebc9a353e052eddb313beec3c3
Omaroookolosio94 <omarookolosio94@gmail.com>
2020-05-29 06:50:19 +0100

    animation for error page

--- client/src/pages/public/Error.jsx
+++ client/src/pages/public/Error.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { ErrorWrapper, ErrorMsg } from '../../components/Fixed/ErrorStyled';
+import {
+  ErrorWrapper,
+  ErrorMsg,
+  Cartoon
+} from '../../components/Fixed/ErrorStyled';
 import { Link } from 'react-router-dom';
 import { Icon, Button } from '../../components/Fixed/Styled';
 
@@ -11,8 +15,15 @@ const Error = () => {
       <ErrorWrapper theme={theme}>
         <ErrorMsg theme={theme}>
           <div className="error">
-            <h1>404</h1>
+            {/* <h1>404</h1> */}
             <p>Page Not Found</p>
+            <Cartoon theme={theme}>
+              <div className="headChar"></div>
+              <div className="bodyChar"></div>
+              <div className="firstLeg"></div>
+              <div className="secondLeg"></div>
+              <div className="shadowChar"></div>
+            </Cartoon>
             <Button theme={theme} success>
               <Link className="navlink" to="/">
                 <Icon theme={theme}>
